0fb7f52ce1e40780558deef3e85477b5ba9ef4ab
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-04-07 13:34:41 -0700

    Add `SerialMessage.FirmwareVersion`

---
+++ Sources/Swarm/Serial/FirmwareVersion.swift
@@ -0,0 +1,48 @@
+//
+//  FirmwareVersion.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 4/7/23.
+//
+
+import Foundation
+
+public extension SerialMessage {
+    
+    /// Swarm Device Firmware Version
+    struct FirmwareVersion: Equatable, Hashable {
+        
+        public let date: Date
+        
+        public let version: String
+    }
+}
+
+extension SerialMessage.FirmwareVersion: SwarmCodableMessage {
+    
+    public static var messageType: SerialMessageType { .firmwareVersion }
+    
+    internal static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        formatter.timeZone = TimeZone(identifier: "UTC")
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        return formatter
+    }()
+    
+    public init?(body: String) {
+        let components = body.split(separator: ",v")
+        guard components.count == 2,
+            let date = Self.dateFormatter.date(from: String(components[0]))
+            else { return nil }
+        
+        self.date = date
+        self.version = String(components[1])
+    }
+    
+    public var body: String {
+        Self.dateFormatter.string(from: date) + ",v" + version
+    }
+}
+
+extension SerialMessage.FirmwareVersion: RawRepresentable { }
