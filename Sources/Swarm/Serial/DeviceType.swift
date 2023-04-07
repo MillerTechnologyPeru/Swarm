27827a7013219fc1cf96fdd5c37ddff384d1ef0d
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-04-06 22:31:12 -0700

    Add `DeviceType`

---
+++ Sources/Swarm/Serial/DeviceType.swift
@@ -0,0 +1,16 @@
+//
+//  DeviceType.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 4/6/23.
+//
+
+/// Swarm Device Type
+public enum DeviceType: String, CaseIterable {
+    
+    /// Tile
+    case tile       = "TILE"
+    
+    /// M138 / Asset Tracker
+    case m138       = "M138"
+}
