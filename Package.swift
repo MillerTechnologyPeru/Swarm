87bfca7e33d607cdfcbe5efa85b97da4cab32a46
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-04-06 18:42:00 -0700

    Set Swift minimum version to 5.7

--- Package.swift
+++ Package.swift
@@ -1,23 +1,21 @@
-// swift-tools-version: 5.8
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version: 5.7
 import PackageDescription
 
 let package = Package(
     name: "Swarm",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Swarm",
-            targets: ["Swarm"]),
+            targets: ["Swarm"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Swarm"),
+            name: "Swarm"
+        ),
         .testTarget(
             name: "SwarmTests",
-            dependencies: ["Swarm"]),
+            dependencies: ["Swarm"]
+        )
     ]
 )
