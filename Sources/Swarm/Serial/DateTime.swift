2e4577e94ff9dcabad12db60c90f39a4c99d09db
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-04-07 12:06:11 -0700

    Add `SerialMessage.DateTimeResponse`

--- Sources/Swarm/Serial/DateTime.swift
+++ Sources/Swarm/Serial/DateTime.swift
@@ -9,11 +9,11 @@ import Foundation
 import RegexBuilder
 
 public extension SerialMessage {
-    
+        
     /// Set or query the rate for $DT unsolicited report messages for date and time.
     ///
     /// Also can retrieve the most current $DT message.
-    enum DateTimeCommand: Equatable, Hashable, Codable {
+    enum DateTimeCommand: Equatable, Hashable {
         
         /// Repeat most recent $DT message.
         case `repeat`
@@ -23,5 +23,73 @@ public extension SerialMessage {
         
         /// Disable or set rate of $DT messages.
         case setRate(UInt32)
+    }
+}
+
+extension SerialMessage.DateTimeCommand: SwarmEncodableMessage {
+    
+    public static var messageType: SerialMessageType { .dateTime }
+    
+    public var body: String {
+        switch self {
+        case .repeat:
+            return "@"
+        case .getRate:
+            return "?"
+        case let .setRate(rate):
+            return rate.description
+        }
+    }
+}
+
+// MARK: - Response
+
+public extension SerialMessage {
+    
+    enum DateTimeResponse: Equatable, Hashable {
+        
+        case ok
+        case error
+        case rate(UInt32)
+        case dateTime(Date, Flag)
+    }
+}
+
+extension SerialMessage.DateTimeResponse: SwarmDecodableMessage {
+    
+    public static var messageType: SerialMessageType { .dateTime }
+    
+    internal static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMddHHmmss"
+        formatter.timeZone = TimeZone(identifier: "UTC")
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        return formatter
+    }()
+    
+    public init?(body: String) {
+        if body == "OK" {
+            self = .ok
+        } else if body == "ERR" {
+            self = .error
+        } else if let rate = UInt32(body) {
+            self = .rate(rate)
+        } else {
+            let components = body.split(separator: ",")
+            guard components.count == 2,
+                let date = Self.dateFormatter.date(from: String(components[0])),
+                let flag = Flag(rawValue: String(components[1]))
+                else { return nil }
+            self = .dateTime(date, flag)
+        }
+    }
+}
+
+public extension SerialMessage.DateTimeResponse {
+    
+    enum Flag: String, Codable, CaseIterable {
+        
+        case valid = "V"
+        case invalid = "I"
     }
 }
