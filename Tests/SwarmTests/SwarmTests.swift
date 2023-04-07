7ca374baf9c4e7306b0396a909d7c2de480bb024
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-04-07 11:09:57 -0700

    Updated unit tests

--- Tests/SwarmTests/SwarmTests.swift
+++ Tests/SwarmTests/SwarmTests.swift
@@ -19,7 +19,28 @@ final class SwarmTests: XCTestCase {
             "$M138 BOOT,RUNNING*2a",
             "$TD OK,4862137892975*2d",
             "$M138 DATETIME*56",
-            "$CS*10"
+            "$CS*10",
+            "$CS DI=0x000e57,DN=TILE*10",
+            "$DT @*70",
+            "$DT 20190408195123,V*41",
+            "$DT 300*03",
+            "$DT OK*34",
+            "$DT ?*0f",
+            "$DT 60*36",
+            "$FV*10",
+            "$FV 2021-07-16-00:10:21,v1.1.0*74",
+            "$GJ @*6d",
+            "$GJ 1,23*31",
+            "$GJ 3600*28",
+            "$GJ OK*29",
+            "$GJ ?*12",
+            "$GJ 10*2c",
+            "$GN @*69",
+            "$GN 37.8921,-122.0155,77,89,2*01",
+            "$GN 30*2a",
+            "$GN OK*2d",
+            "$GN ?*16",
+            "$GN 15*2d"
         ]
         
         for string in messages {
