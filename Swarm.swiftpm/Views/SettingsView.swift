0bde059270c7224a242990c26eeebf85e73c4b96
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-04-08 08:14:37 -0700

    Add `SettingsView`

---
+++ Swarm.swiftpm/Views/SettingsView.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsView.swift
+//  SwarmApp
+//
+//  Created by Alsey Coleman Miller on 4/8/23.
+//
+
+import Foundation
+import SwiftUI
+import Swarm
+
+struct SettingsView: View {
+    
+    var body: some View {
+        List {
+            Section {
+                
+            }
+        }
+        .listStyle(.grouped)
+        .navigationTitle("Settings")
+    }
+}
