a2abc5c1ec1aaf3dc47690bca14da7c65b393e6b
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-04-07 23:08:03 -0700

    Add `AuthorizationToken`

---
+++ Sources/Swarm/Networking/AuthorizationToken.swift
@@ -0,0 +1,45 @@
+//
+//  AuthorizationToken.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 4/7/23.
+//
+
+import Foundation
+
+/// Swarm Authorization Token
+public struct AuthorizationToken: Equatable, Hashable, RawRepresentable, Codable {
+    
+    public let rawValue: String
+    
+    public init(rawValue: String) {
+        self.rawValue = rawValue
+    }
+}
+
+// MARK: - CustomStringConvertible
+
+extension AuthorizationToken: CustomStringConvertible {
+    
+    public var description: String {
+        rawValue
+    }
+}
+
+// MARK: - ExpressibleByStringLiteral
+
+extension AuthorizationToken: ExpressibleByStringLiteral {
+    
+    public init(stringLiteral value: String) {
+        self.init(rawValue: value)
+    }
+}
+
+// MARK: - HTTP Header
+
+public extension URLRequest {
+    
+    mutating func setAuthorization(_ token: AuthorizationToken) {
+        self.setValue("Authorization: Bearer " + token.rawValue, forHTTPHeaderField: "Authorization")
+    }
+}
