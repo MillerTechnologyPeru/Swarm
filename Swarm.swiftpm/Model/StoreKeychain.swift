f1f800c5159bc76bbf1ad9eed4afe7be58364988
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-04-08 06:46:44 -0700

    Add `InMemoryCredentialStorage`

--- Swarm.swiftpm/Model/StoreKeychain.swift
+++ Swarm.swiftpm/Model/StoreKeychain.swift
@@ -9,27 +9,84 @@ import Foundation
 import Swarm
 import KeychainAccess
 
+internal protocol CredentialStorage: AnyObject {
+    
+    func removeAll() throws
+    
+    func get(_ key: String) throws -> AuthorizationToken?
+    
+    func remove(_ key: String) throws
+    
+    func contains(_ key: String) throws -> Bool
+    
+    func set(_ newValue: AuthorizationToken, key: String) throws
+}
+
+extension KeychainAccess.Keychain: CredentialStorage {
+    
+    func get(_ key: String) throws -> Swarm.AuthorizationToken? {
+        try getString(key, ignoringAttributeSynchronizable: true).flatMap { AuthorizationToken(rawValue: $0) }
+    }
+    
+    func remove(_ key: String) throws {
+        try remove(key, ignoringAttributeSynchronizable: true)
+    }
+    
+    func contains(_ key: String) throws -> Bool {
+        try contains(key, withoutAuthenticationUI: true)
+    }
+    
+    func set(_ newValue: Swarm.AuthorizationToken, key: String) throws {
+        try set(newValue.rawValue, key: key, ignoringAttributeSynchronizable: true)
+    }
+}
+
+final class InMemoryCredentialStorage: CredentialStorage {
+    
+    var tokens = [String: AuthorizationToken]()
+    
+    init() { }
+    
+    func removeAll() {
+        tokens.removeAll(keepingCapacity: true)
+    }
+    
+    func get(_ key: String) -> Swarm.AuthorizationToken? {
+        tokens[key]
+    }
+    
+    func remove(_ key: String) {
+        tokens[key] = nil
+    }
+    
+    func contains(_ key: String) -> Bool {
+        tokens.keys.contains(where: { $0 == key })
+    }
+    
+    func set(_ newValue: Swarm.AuthorizationToken, key: String) {
+        tokens[key] = newValue
+    }
+}
+
 internal extension Store {
     
-    func loadKeychain() -> Keychain {
+    func loadKeychain() -> CredentialStorage {
+        #if KEYCHAIN
         let keychain = Keychain(service: "com.colemancda.Swarm.AuthorizationToken")
+        #else
+        let keychain = InMemoryCredentialStorage()
+        #endif
         // reset if new installation
         clearKeychainNewInstall()
         return keychain
     }
     
-    // FIXME: Fix keychain
-    subscript (token username: String) -> AuthorizationToken? {
-        get { self.token }
-        set { self.token = newValue }
-    }
-    /*
     subscript (token username: String) -> AuthorizationToken? {
         get {
             do {
-                guard let token = try keychain.getString(username)
+                guard let token = try keychain.get(username)
                     else { return nil }
-                return AuthorizationToken(rawValue: token)
+                return token
             } catch {
                 #if DEBUG
                 log(error.localizedDescription)
@@ -41,7 +98,7 @@ internal extension Store {
         
         set {
             do {
-                guard let token = newValue?.rawValue else {
+                guard let token = newValue else {
                     try keychain.remove(username)
                     return
                 }
@@ -57,7 +114,7 @@ internal extension Store {
                 assertionFailure("Unable store value in keychain: \(error)")
             }
         }
-    }*/
+    }
 }
 
 private extension Store {
@@ -71,7 +128,7 @@ private extension Store {
             catch {
                 log("⚠️ Unable to clear keychain: \(error.localizedDescription)")
                 // TODO: Flush logs
-                //assertionFailure("Unable to clear keychain")
+                assertionFailure("Unable to clear keychain")
             }
         }
     }
