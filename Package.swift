6c6a5fc1f4bda228c9bbca21a1f50bf6c456114f
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-04-07 23:43:17 -0700

    Rename executable

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     products: [
         .executable(
-            name: "swarm",
+            name: "swarmtool",
             targets: ["SwarmTool"]
         ),
         .library(
