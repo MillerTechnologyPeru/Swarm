c3a9350eb25a7ab2b1cf3ab6739bcb586c16c971
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-04-07 23:42:37 -0700

    Updated unit tests

--- Tests/SwarmTests/NetworkingTests.swift
+++ Tests/SwarmTests/NetworkingTests.swift
@@ -16,24 +16,48 @@ final class NetworkingTests: XCTestCase {
         let request = LoginRequest(username: "colemancda", password: "1234")
         let response = LoginResponse(token: "Very1Long_random.L00King2String")
         
-        let url = URL(string: "https://bumblebee.hive.swarm.space/hive.login?username=colemancda&password=1234")!
-        var urlRequest = URLRequest(url: url)
-        urlRequest.httpMethod = "POST"
-        let responseJSON = #"""
-        {
-          "token": "Very1Long_random.L00King2String"
-        }
-        """#
-        let urlResponse = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
-        var client = MockClient()
-        client.responses[urlRequest] = (Data(responseJSON.utf8), urlResponse)
+        let url = URL(string: "https://bumblebee.hive.swarm.space/hive/login?username=colemancda&password=1234")!
+        XCTAssertEqual(request.url(for: .production), url)
+        
+        let client: MockClient = {
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = "POST"
+            let responseJSON = #"""
+            {
+              "token": "Very1Long_random.L00King2String"
+            }
+            """#
+            let urlResponse = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
+            var client = MockClient()
+            client.responses[urlRequest] = (Data(responseJSON.utf8), urlResponse)
+            return client
+        }()
+        
         let token = try await client.login(
             username: request.username,
             password: request.password
         )
         
         XCTAssertEqual(token, response.token)
-        XCTAssertEqual(request.url(for: .production), url)
+    }
+    
+    func testLogout() async throws {
+        
+        let token: AuthorizationToken = "Very1Long_random.L00King2String"
+        let url = URL(string: "https://bumblebee.hive.swarm.space/hive/logout")!
+        XCTAssertEqual(LogoutRequest().url(for: .production), url)
+        
+        let client: MockClient = {
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = "GET"
+            urlRequest.setValue("Bearer Very1Long_random.L00King2String", forHTTPHeaderField: "Authorization")
+            let urlResponse = HTTPURLResponse(url: url, statusCode: 204, httpVersion: nil, headerFields: nil)!
+            var client = MockClient()
+            client.responses[urlRequest] = (Data(), urlResponse)
+            return client
+        }()
+        
+        try await client.logout(authorization: token)
     }
 }
 
