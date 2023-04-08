fdd247ee5802363d6884e87b2950be26506239f2
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-04-07 23:08:59 -0700

    Updated unit tests

--- Tests/SwarmTests/NetworkingTests.swift
+++ Tests/SwarmTests/NetworkingTests.swift
@@ -11,10 +11,44 @@ import XCTest
 
 final class NetworkingTests: XCTestCase {
     
-    func testLogin() {
+    func testLogin() async throws {
         
         let request = LoginRequest(username: "colemancda", password: "1234")
-        XCTAssertEqual(URLRequest(request: request, server: .production).url?.absoluteString, "https://bumblebee.hive.swarm.space/hive.login?username=colemancda&password=1234")
+        let response = LoginResponse(token: "Very1Long_random.L00King2String")
         
+        let url = URL(string: "https://bumblebee.hive.swarm.space/hive.login?username=colemancda&password=1234")!
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        let responseJSON = #"""
+        {
+          "token": "Very1Long_random.L00King2String"
+        }
+        """#
+        let urlResponse = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
+        var client = MockClient()
+        client.responses[urlRequest] = (Data(responseJSON.utf8), urlResponse)
+        let token = try await client.login(
+            username: request.username,
+            password: request.password
+        )
+        
+        XCTAssertEqual(token, response.token)
+        XCTAssertEqual(request.url(for: .production), url)
+    }
+}
+
+// MARK: - Supporting Types
+
+struct MockClient: HTTPClient {
+    
+    var responses = [URLRequest: (Data, HTTPURLResponse)]()
+    
+    init() { }
+    
+    func data(for request: URLRequest) throws -> (Data, URLResponse) {
+        guard let response = responses[request] else {
+            throw URLError(.resourceUnavailable)
+        }
+        return response
     }
 }
