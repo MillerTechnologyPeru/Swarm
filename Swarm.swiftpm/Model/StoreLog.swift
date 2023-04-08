61fa5a3247787f4b076682b0bb1623c7e136d09e
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-04-08 00:43:53 -0700

    Add `Store.log()`

---
+++ Swarm.swiftpm/Model/StoreLog.swift
@@ -0,0 +1,16 @@
+//
+//  StoreLog.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 4/8/23.
+//
+
+import Foundation
+
+public extension Store {
+    
+    func log(_ message: String) {
+        print(message)
+    }
+    
+}
