fbbe53e33f69997ee4808024bfb1d4e8b630ff37
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-04-08 23:18:06 -0700

    Add `Packet.Status`

--- Sources/Swarm/Networking/Packet.swift
+++ Sources/Swarm/Networking/Packet.swift
@@ -11,10 +11,15 @@ import Foundation
 public struct Packet: Equatable, Hashable, Codable, Identifiable {
     
     /// A unique identifier for the packet.
-    public let id: UInt
+    public let id: UInt64
     
-    /// A unique identifier for the message contained within the packet.
-    public let messageId: UInt
+    /// Swarm message ID.
+    ///
+    /// There may be multiple messages for a single message ID.
+    /// A message ID represents an intent to send a message, but there may be multiple Swarm packets that are required to fulfill that intent.
+    ///
+    /// For example, if a Hive -> device message fails to reach its destination, automatic retry attempts to send that message will have the same message ID.
+    public let messageId: UInt64
     
     /// An identifier for the type of device that sent the packet.
     public let deviceType: UInt
@@ -41,10 +46,10 @@ public struct Packet: Equatable, Hashable, Codable, Identifiable {
     public let payload: Data
     
     /// A unique identifier for the acknowledgment packet associated with this packet.
-    public let ackPacketId: Int
+    public let ackPacketId: Packet.ID
     
-    /// The status of the packet (0 indicates that it was successfully received).
-    public let status: Int
+    /// The status of the packet.
+    public let status: Status
     
     /// The time at which the packet was received by the server (in UTC timezone).
     public let hiveRxTime: Date
@@ -65,3 +70,56 @@ public struct Packet: Equatable, Hashable, Codable, Identifiable {
         case hiveRxTime
     }
 }
+
+// MARK: - Supporting Types
+
+public extension Packet {
+    
+    /// An enumeration representing the status of a message packet.
+    enum Status: Int, Codable, CaseIterable {
+        
+        /// The packet represents an incoming message from a device.
+        case incoming = 0
+        
+        /// The packet represents an outgoing message to a device.
+        case outgoing = 1
+        
+        /// The packet represents an incoming message that has been acknowledged as seen by the customer.
+        case incomingAcknowledged = 2
+        
+        /// The packet represents an outgoing message that is currently on a groundstation.
+        case outgoingOnGroundStation = 3
+        
+        /// An error occurred while processing the packet, and it cannot be delivered.
+        case error = -1
+        
+        /// A delivery attempt has failed, and the system is retrying the delivery.
+        case deliveryRetry = -3
+        
+        /// A delivery attempt has failed, and the system will not attempt to deliver the packet again.
+        case deliveryFailed = -4
+    }
+}
+
+extension Packet.Status: CustomStringConvertible {
+    
+    public var description: String {
+        switch self {
+        case .incoming:
+            return "Incoming message"
+        case .outgoing:
+            return "Outgoing message"
+        case .incomingAcknowledged:
+            return "Incoming message acknowledged"
+        case .outgoingOnGroundStation:
+            return "Outgoing message on Ground Station"
+        case .error:
+            return "Error"
+        case .deliveryRetry:
+            return "Delivery retry"
+        case .deliveryFailed:
+            return "Delivery failed"
+        }
+    }
+}
+
