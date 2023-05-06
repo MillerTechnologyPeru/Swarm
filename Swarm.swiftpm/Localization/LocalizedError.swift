534bf908fdc3b490d4379a256e3f247405ace8d2
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-05-06 16:00:45 -0700

    Add `SwarmNetworkingError.errorDescription`

---
+++ Swarm.swiftpm/Localization/LocalizedError.swift
@@ -0,0 +1,23 @@
+//
+//  LocalizedError.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 5/6/23.
+//
+
+import Foundation
+import Swarm
+
+extension SwarmNetworkingError: LocalizedError {
+    
+    public var errorDescription: String? {
+        switch self {
+        case .authenticationRequired:
+            return NSLocalizedString("Authentication required.", comment: "Authentication required.")
+        case .invalidResponse:
+            return NSLocalizedString("Invalid server response.", comment: "Invalid server response.")
+        case let .invalidStatusCode(code):
+            return String(format: NSLocalizedString("Invalid status code %@.", comment: "Invalid status code %@."), "\(code.description)")
+        }
+    }
+}
