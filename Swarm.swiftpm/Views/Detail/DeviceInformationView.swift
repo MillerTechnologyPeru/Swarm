b7e1eabb8445e7381a258de2108c85ae16ac06c2
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-05-07 10:57:18 -0700

    Updated `DeviceInformationView`

--- Swarm.swiftpm/Views/Detail/DeviceInformationView.swift
+++ Swarm.swiftpm/Views/Detail/DeviceInformationView.swift
@@ -95,31 +95,116 @@ extension DeviceInformationView {
         
         var body: some View {
             List {
-                SubtitleRow(
-                    title: Text("Identifier"),
-                    subtitle: Text(verbatim: device.id.description)
-                )
-                if device.comments.isEmpty == false {
-                    Text(verbatim: device.comments)
+                Section {
+                    SubtitleRow(
+                        title: Text("Identifier"),
+                        subtitle: Text(verbatim: device.id.description)
+                    )
+                    if device.comments.isEmpty == false {
+                        SubtitleRow(
+                            title: Text("Description"),
+                            subtitle: Text(verbatim: device.comments)
+                        )
+                    }
+                    SubtitleRow(
+                        title: Text("Device Type"),
+                        subtitle: Text(verbatim: device.deviceType.description)
+                    )
+                    SubtitleRow(
+                        title: Text("Firmware Version"),
+                        subtitle: Text(verbatim: device.firmwareVersion.description)
+                    )
+                    if device.hardwareVersion.isEmpty == false {
+                        SubtitleRow(
+                            title: Text("Hardware Version"),
+                            subtitle: Text(verbatim: device.hardwareVersion)
+                        )
+                    }
                 }
-                SubtitleRow(
-                    title: Text("Firmware Version"),
-                    subtitle: Text(verbatim: device.firmwareVersion.description)
-                )
-                SubtitleRow(
-                    title: Text("Created"),
-                    subtitle: Text(verbatim: device.hiveCreationTime.formatted())
-                )
-                SubtitleRow(
-                    title: Text("First Heard"),
-                    subtitle: Text(verbatim: device.hiveFirstheardTime.formatted())
-                )
-                SubtitleRow(
-                    title: Text("Last Heard"),
-                    subtitle: Text(verbatim: device.hiveLastheardTime.formatted())
-                )
+                
+                Section {
+                    SubtitleRow(
+                        title: Text("Created"),
+                        subtitle: Text(verbatim: device.hiveCreationTime.formatted())
+                    )
+                    SubtitleRow(
+                        title: Text("First Heard"),
+                        subtitle: Text(verbatim: device.hiveFirstheardTime.formatted())
+                    )
+                    SubtitleRow(
+                        title: Text("Last Heard"),
+                        subtitle: Text(verbatim: device.hiveLastheardTime.formatted())
+                    )
+                    if let uptime = device.uptime {
+                        SubtitleRow(
+                            title: Text("Uptime"),
+                            subtitle: Text("\(uptime)s")
+                        )
+                    }
+                    SubtitleRow(
+                        title: Text("Status"),
+                        subtitle: Text(verbatim: device.status.description)
+                    )
+                    SubtitleRow(
+                        title: Text("Two Way Enabled"),
+                        subtitle: Text(verbatim: device.twoWayEnabled.description)
+                    )
+                    SubtitleRow(
+                        title: Text("Data Encryption Enabled"),
+                        subtitle: Text(verbatim: device.dataEncryptionEnabled.description)
+                    )
+                }
+                
+                NavigationLink("Messages") {
+                    MessagesView(device: device.id)
+                }
             }
             .navigationTitle(device.deviceName)
         }
     }
 }
+
+#if DEBUG
+struct DeviceInformationView_Previews: PreviewProvider {
+    
+    static let responseJSON = #"""
+        [
+          {
+            "deviceType": 1,
+            "deviceId": 27662,
+            "deviceName": "My Swarm Device",
+            "comments": "Tracker",
+            "hiveCreationTime": "2022-10-04T18:46:02",
+            "hiveFirstheardTime": "2023-03-26T14:30:51",
+            "hiveLastheardTime": "2023-04-06T20:00:35",
+            "firmwareVersion": "v3.0.1",
+            "hardwareVersion": "",
+            "lastTelemetryReportPacketId": 53906350,
+            "lastHeardByDeviceType": 3,
+            "lastHeardByDeviceId": 1530,
+            "counter": 0,
+            "dayofyear": 0,
+            "lastHeardCounter": 98,
+            "lastHeardDayofyear": 96,
+            "lastHeardByGroundstationId": 248872,
+            "status": 0,
+            "twoWayEnabled": false,
+            "dataEncryptionEnabled": true,
+            "metadata": {
+              "device:type": "Other"
+            }
+          }
+        ]
+        """#
+    
+    static let device = try! JSONDecoder.swarm.decode([DeviceInformation].self, from: Data(responseJSON.utf8)).first!
+    
+    static var previews: some View {
+        Group {
+            NavigationView {
+                DeviceInformationView.StateView(device: device)
+            }
+        }
+    }
+}
+#endif
