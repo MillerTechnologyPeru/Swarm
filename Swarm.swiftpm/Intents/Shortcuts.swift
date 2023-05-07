8c5c8af2b3b879f663f259ddac6259580e75252a
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-05-06 20:20:53 -0700

    Add Siri Shortcut

--- Swarm.swiftpm/Intents/Shortcuts.swift
+++ Swarm.swiftpm/Intents/Shortcuts.swift
@@ -7,13 +7,20 @@
 
 import Foundation
 import AppIntents
-/*
-@available(iOS 16.0, *)
+
+@available(macOS 13.0, iOS 16.0, *)
 struct AppShortcuts: AppShortcutsProvider {
     
     /// The background color of the tile that Shortcuts displays for each of the app's App Shortcuts.
     static var shortcutTileColor: ShortcutTileColor { .navy }
-
-    static var appShortcuts: [AppShortcut] {  }
+    
+    static var appShortcuts: [AppShortcut] {
+        AppShortcut(
+            intent: FetchMessagesIntent(device: nil, limit: 0),
+            phrases: [
+                "Fetch last message with \(.applicationName)",
+            ],
+            systemImageName: "envelope"
+        )
+    }
 }
- */
