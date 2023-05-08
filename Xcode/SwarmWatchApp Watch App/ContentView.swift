8b93648552d59f490773906eca2eea2c08c59ee5
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-05-07 20:06:36 -0700

    Created watchOS app

---
+++ Xcode/SwarmWatchApp Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwarmWatchApp Watch App
+//
+//  Created by Alsey Coleman Miller on 5/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Xcode/SwarmWatchApp Watch App/SwarmWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwarmWatchAppApp.swift
+//  SwarmWatchApp Watch App
+//
+//  Created by Alsey Coleman Miller on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SwarmWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
