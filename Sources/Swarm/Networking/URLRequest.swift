d0acbaab5d3fc61af351d9f1ff3ce9c12d7b0573
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-05-09 10:30:49 -0700

    Fixed `HTTPClient`

--- Sources/Swarm/Networking/URLRequest.swift
+++ Sources/Swarm/Networking/URLRequest.swift
@@ -70,7 +70,7 @@ public extension HTTPClient {
             fatalError("Invalid response type \(urlResponse)")
         }
         guard httpResponse.statusCode == statusCode else {
-            let errorResponse = try? ErrorResponse(from: data)
+            let errorResponse = data.isEmpty ? nil : try? ErrorResponse(from: data)
             throw SwarmNetworkingError.invalidStatusCode(httpResponse.statusCode, errorResponse)
         }
         return data
