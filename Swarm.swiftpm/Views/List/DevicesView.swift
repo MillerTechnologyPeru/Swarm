3e9aaef4d3fdd904ea5700b2af8af688b7d4d000
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-05-09 20:05:53 -0700

    Add preview

--- Swarm.swiftpm/Views/List/DevicesView.swift
+++ Swarm.swiftpm/Views/List/DevicesView.swift
@@ -236,3 +236,50 @@ private extension DevicesView.StateView {
         })
     }
 }
+
+
+#if DEBUG
+struct DevicesView_Previews: PreviewProvider {
+    
+    static let responseJSON = #"""
+        [
+          {
+            "deviceType": 1,
+            "deviceId": 12345,
+            "deviceName": "My Swarm Device",
+            "comments": "Tracker",
+            "hiveCreationTime": "2022-10-04T18:46:02",
+            "hiveFirstheardTime": "2023-03-26T14:30:51",
+            "hiveLastheardTime": "2023-04-06T20:00:35",
+            "firmwareVersion": "v3.0.1",
+            "hardwareVersion": "",
+            "lastTelemetryReportPacketId": 53906350,
+            "lastHeardByDeviceType": 3,
+            "lastHeardByDeviceId": 1530,
+            "counter": 0,
+            "dayofyear": 0,
+            "lastHeardCounter": 98,
+            "lastHeardDayofyear": 96,
+            "lastHeardByGroundstationId": 248872,
+            "status": 0,
+            "twoWayEnabled": false,
+            "dataEncryptionEnabled": true,
+            "metadata": {
+              "device:type": "Other"
+            }
+          }
+        ]
+        """#
+    
+    static let devices = try! JSONDecoder.swarm.decode([DeviceInformation].self, from: Data(responseJSON.utf8))
+    
+    static var previews: some View {
+        NavigationView {
+            DevicesView.StateView(
+                devices: devices,
+                selection: .constant(nil)
+            )
+        }
+    }
+}
+#endif
